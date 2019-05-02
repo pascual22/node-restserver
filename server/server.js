a140a371298d7d383a0f4c9b9ba703de0dd28913
Oscar Gonzalez <osmaogonzalez@gmail.com>
2019-05-01 23:04:22 -0500

    ajuste npm start puerto

--- server/server.js
+++ server/server.js
@@ -36,6 +36,6 @@ app.delete('/usuario', function (req, res){
     res.json('delete usuario');
 })
 
-app.listen(3000, () => {
+app.listen(process.env.PORT00, () => {
     console.log('Escuchando el puerto ', process.env.PORT);
 });
